99d6fa37baf0648ff77b0f04b82f657f70c05ce8
Ilya G <ilya.golyashov@yandex.ru>
2023-05-18 14:12:56 +0300

    18.05

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,21 +1,31 @@
-function handleSubmit(event) {
+/* function handleSubmit(event) {
     event.preventDefault();
     const form = document.getElementById("quizForm");
     const selectedAnswer = form.elements["answer"].value;
     console.log("Выбранный ответ:", selectedAnswer);
     form.submit();
-}
+} */
+
+/* function activateRadio(element) {
+    const radio = element.querySelector('input[type="radio"]');
+    radio.checked = true;
+} */
 
 function activateRadio(element) {
     const radio = element.querySelector('input[type="radio"]');
-    radio.checked = true;
-}
 
-function activateRadio(element) {
-    const radio = element.querySelector('input[type="radio"]');
     radio.checked = true;
-
-    const items = document.querySelectorAll('.answers__item');
+    const items = document.querySelectorAll('.answer');
     items.forEach(item => item.classList.remove('selected'));
     element.classList.add('selected');
 }
+
+/* function activateNextBtn () {
+    const btnNext = document.querySelector('.quiz__btn');
+    const items = document.querySelectorAll('.answer');
+    const radios = document.querySelectorAll('input[type="radio"]');
+    
+    radios.forEach(radio => {
+        
+    })
+} */
