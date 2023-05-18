a28ebce3e6c967e42eb279d0205cb5115a5315b5
Ilya G <ilya.golyashov@yandex.ru>
2023-05-18 15:19:06 +0300

    18.05 2.0

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,19 +13,30 @@
 
 function activateRadio(element) {
     const radio = element.querySelector('input[type="radio"]');
-
+  
     radio.checked = true;
     const items = document.querySelectorAll('.answer');
     items.forEach(item => item.classList.remove('selected'));
     element.classList.add('selected');
-}
-
-/* function activateNextBtn () {
+  }
+  document.querySelectorAll('.answer').forEach(answer => {
+    answer.addEventListener('click', function() {
+      activateRadio(this);
+    });
+  });
+  
+/*   function activateNextBtn() {
     const btnNext = document.querySelector('.quiz__btn');
-    const items = document.querySelectorAll('.answer');
-    const radios = document.querySelectorAll('input[type="radio"]');
-    
-    radios.forEach(radio => {
-        
-    })
-} */
+    const items = [...document.querySelectorAll('.answer')];
+    const cross = document.querySelector('.cross-circle');
+  
+    if (items.some(i => i.classList.contains('selected'))) {
+        btnNext.setAttribute('class', 'start__btn quiz__btn quiz__btn--active');
+        cross.classList.add('cross-circle--active');
+    }
+  }
+  
+  document.addEventListener('DOMContentLoaded', () => {
+        activateNextBtn();
+  });
+   */
